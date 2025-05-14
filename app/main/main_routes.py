42fd8c6ab3fc30b47677b359ecdad021f45ffad9
listsarah <sarah.listzwan@gmail.com>
2025-05-13 20:43:26 -0400

    in progress

--- app/main/main_routes.py
+++ app/main/main_routes.py
@@ -1,6 +1,14 @@
 from flask import render_template
+from app import db
+import sqlalchemy as sqla
+from app.main.main_models import Project
 from app.main import main_blueprint as bp_main
 
 @bp_main.route('/', methods=['GET'])
 def index():
     return render_template('main.html')
+
+@bp_main.route('/projects/retreive', methods=['GET'])
+def get_projects():
+    projects = db.session.scalars(sqla.select(Project))
+    return # make projects as json
