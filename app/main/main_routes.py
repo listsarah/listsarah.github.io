4931945bf8a6090358f5d63f74de606654350210
listsarah <sarah.listzwan@gmail.com>
2025-05-15 22:42:38 -0400

    removed all db stuff

--- app/main/main_routes.py
+++ app/main/main_routes.py
@@ -1,7 +1,4 @@
 from flask import render_template
-from app import db
-import sqlalchemy as sqla
-from app.main.main_models import Project
 from app.main import main_blueprint as bp_main
 
 @bp_main.route('/', methods=['GET'])
@@ -10,7 +7,7 @@ def index():
 
 @bp_main.route('/projects/retreive', methods=['GET'])
 def get_projects():
-    projects = db.session.scalars(sqla.select(Project))
+    projects = [] # read json here
     projects_json = []
     for project in projects:
         projects_json.append(project.export())
