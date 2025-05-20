fbfde607b18156988d2f3212198d958563f2021b
listsarah <sarah.listzwan@gmail.com>
2025-05-20 10:45:12 -0400

    fixed admin

--- config.py
+++ config.py
@@ -10,6 +10,5 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ROOT_PATH = basedir
     TEMPLATE_FOLDER_MAIN = os.path.join(basedir, 'app//main//templates')
-    TEMPLATE_FOLDER_ADMIN = os.path.join(basedir, 'app//admin//templates')
     TEMPLATE_FOLDER_ERRORS = os.path.join(basedir, 'app//errors//templates') 
     MAIN_STATIC_FOLDER = os.path.join(basedir, 'app//static')    
